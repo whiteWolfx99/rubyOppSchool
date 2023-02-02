5c5cbaabd2143a659c59cc8b36a28f419c537cf1
whiteWolfx99 <hhqu180099@uniq.edu.iq>
2023-02-02 09:17:50 +0300

    add decorate class

--- person.rb
+++ person.rb
@@ -1,27 +1,29 @@
+require_relative 'nameable'
 # Class that represents a person
+
 class Person
+  include Nameable
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(age, name: 'Unknown', parent_permission: true)
-    @id = rand(1..1000)
-
+  def initialize(id, age, name: 'Unknown', parent_permission: true)
+    @id = id
     @name = name
-
     @age = age
-
     @parent_permission = parent_permission
+  end
+
+  def can_use_services?
+    is_of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   private
 
-  def of_age?
+  def is_of_age?
     @age >= 18
-  end
-
-  public
-
-  def can_use_services?
-    is_of_age? || @parent_permission
   end
 end
